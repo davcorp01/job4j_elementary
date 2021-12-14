2d4f37a5ae24e7a5faed3c6a3cd4b840f46f58da
volkov.d <dmitriy.a.volkov@gmail.com>
2021-12-14 19:36:15 +0500

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/condition/Types.java
+++ src/main/java/ru/job4j/condition/Types.java
@@ -2,9 +2,26 @@ package ru.job4j.condition;
 
 public class Types {
     public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println("#1 Было: " + l + " Стало: " + b);
+
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println("#2 Было: " + (f + i) + " Стало: " + rsl);
+
+        char c = 45000;
+        float ii = c;
+        System.out.println("#3 Было: " + c + " Стало: " + ii);
+
+        double d = 121.19;
+        byte bb = (byte) d;
+        System.out.println("#4 Было: " + d + " Стало: " + bb);
+
         short s = 1500;
-        char c = (char) s;
-        double d = c;
-        System.out.println("Вывод в консоль значения после расширения: " + d);
+        char cc = (char) s;
+        double dd = cc;
+        System.out.println("#5 Было: " + s + " Потом: " + cc + " Стало: " + dd);
     }
 }
