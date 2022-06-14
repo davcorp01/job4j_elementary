866f3a3d05ae467800c986a0ddba976b4396d880
volkov.d <dmitriy.a.volkov@gmail.com>
2022-06-14 12:56:36 +0500

    0.2. Первое больше второго

---
+++ src/main/java/ru/job4j/condition/GreatThen.java
@@ -0,0 +1,8 @@
+package ru.job4j.condition;
+
+public class GreatThen {
+    public static boolean check(int first, int second) {
+        return first > second;
+        }
+    }
+
---
+++ src/test/java/ru/job4j/condition/GreatThenTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GreatThenTest {
+
+    @Test
+    public void whenFirstLess() {
+        assertThat(GreatThen.check(1, 2), is(false));
+    }
+
+    @Test
+    public void whenFirstGreat() {
+        assertThat(GreatThen.check(10, 2),
+                is(true)
+        );
+    }
+}
