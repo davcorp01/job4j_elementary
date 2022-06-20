ddf688213192802afbda7d70ad3d9c6e7ff6164c
volkov.d <dmitriy.a.volkov@gmail.com>
2022-06-20 11:28:58 +0500

    5.7. Ипотека [#156315 #314366]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double duty = amount * (percent / 100 + 1);
-        while (duty >= salary) {
-            duty = (duty - salary) * (percent / 100 + 1);
+        int year = 0;
+        amount = amount * (percent / 100 + 1);
+        while (amount >= 0) {
+           amount = (amount - salary) * (percent / 100 + 1);
            year++;
         }
         return year;
