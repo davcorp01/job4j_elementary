a3273df8766e87026886a0e00717d021bca7183b
davcorp01 <dmitriy.a.volkov@gmail.com>
2022-11-23 08:19:04 +0500

    тест из дома

--- src/main/java/ru/job4j/loop/BackwardFor.java
+++ src/main/java/ru/job4j/loop/BackwardFor.java
@@ -1,10 +1,18 @@
 package ru.job4j.loop;
 
-/*выаыва*/
+/*
 public class BackwardFor {
     public static void out(int m, int n) {
         for (int index = m; index > n; index--) {
             System.out.println(index);
+        }
+    }*/
+
+/*выаыва*/
+public class BackwardFor {
+    public static void out(int m, int n) {
+        for (int index = m; index > n; index--) {
+            System.out.println(index);/*бтбьтбюьт
         }
     }
 }
