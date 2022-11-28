17a1648855b5d2430d442520512f8e93e9ada316
volkov.d <dmitriy.a.volkov@gmail.com>
2022-11-28 16:45:07 +0500

    6. DSL c AssertJ [#504879 #365267]

---
+++ src/main/java/ru/job4j/io/Task15.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+public class Task15 {
+    public static void main(String[] args) {
+        String source = "+!?";
+        System.out.print(source.charAt(0));
+        System.out.print(source.charAt(0));
+        System.out.println(source.charAt(0));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.println(source.charAt(1));
+        System.out.print(source.charAt(2));
+        System.out.print(source.charAt(2));
+
+        // YOUR CODE
+    }
+}
