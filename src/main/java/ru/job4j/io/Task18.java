b17946a82f5ade76e8b66024d4930b9d4d30eb48
volkov.d <dmitriy.a.volkov@gmail.com>
2022-11-28 18:15:23 +0500

    Task**

---
+++ src/main/java/ru/job4j/io/Task18.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+public class Task18 {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        //double a = input.nextDouble();
+        //double r = input.nextDouble();
+        double a = 33.3;
+        double r = 33.3;
+        //double r = input.nextDouble();
+        // периметр квадрата
+        System.out.printf("%.1f%n", a * 4);
+        // длина окружности
+        System.out.printf("%.1f%n", 2 * Math.PI * r);
+        // площадь круга
+        System.out.printf("%.1f%n", Math.PI * (r * r));
+    }
+}
