575df7a1077928037f135240b205e22cad4e905f
volkov.d <dmitriy.a.volkov@gmail.com>
2022-11-29 14:52:10 +0500

    Task169_1

--- src/main/java/ru/job4j/loop/Task169_1.java
+++ src/main/java/ru/job4j/loop/Task169_1.java
@@ -2,43 +2,44 @@ package ru.job4j.loop;
 
 import javax.swing.*;
 
-public class Task169 {
+public class Task169_1 {
     public static void paint(int columnCount) {
         String star = "*";
         String space = " ";
         int dotX = (columnCount + 1);
-        for (int i = 1; i <= columnCount; i++) {
-            if (i == 1 || i == columnCount) {
+        for (int r = 1; r <= columnCount; r++) {
+            if (r == 1 || r == columnCount) {
                 for (int j = 1; j <= columnCount; j++) {
                     System.out.print(star);
                 }
-                if (i != columnCount) {
+                if (r != columnCount) {
                     System.out.println();
                 }
             }
-            if (i != 1 && i != columnCount) {
-                for (int rowCount = 1; rowCount <= columnCount; rowCount++) {
-                    if (rowCount == 1 || rowCount == columnCount) {
-                        System.out.print(space);
+            if (r != 1 && r != columnCount) {
+                System.out.print(space);
+                for (int c = 2; c < columnCount; c++) {
+                    if (c == 1 || c == columnCount) {
+
                         continue;
                     }
-                    if (i == rowCount && dotX / 2 > rowCount) {
+                    if (r == c && dotX / 2 > c) {
                         System.out.print(star);
                         continue;
                     }
-                    if (dotX == rowCount + i && dotX / 2 > rowCount) {
+                    if (dotX == c + r && dotX / 2 > c) {
                         System.out.print(star);
                         continue;
                     }
-                    if (dotX == rowCount + i) {
+                    if (dotX == c + r) {
                         System.out.print(star);
                         break;
                     }
-                    if (i == rowCount && dotX / 2 < rowCount) {
+                    if (r == c && dotX / 2 < c) {
                         System.out.print(star);
                         break;
                     }
-                    if (i != rowCount || dotX != rowCount) {
+                    if (r != c || dotX != c) {
                         System.out.print(space);
 
                     }
