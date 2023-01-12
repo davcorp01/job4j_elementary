3225b518459f0338d1b0f5d3d0c007dff75272fa
volkov.d <dmitriy.a.volkov@gmail.com>
2023-01-12 14:37:48 +0500

    Task49

---
+++ src/main/java/ru/job4j/condition/Task49.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+public class Task49 {
+    @SuppressWarnings("checkstyle:RightCurly")
+    public static void isPalindrome(int number) {
+        String s = String.valueOf(number);
+        int iLen = s.length();
+        boolean result = true;
+        for (int i = 0; i < (iLen / 2); i++) {
+            if (s.charAt(i) != s.charAt(iLen - 1 - i)) {
+                result = false;
+                break;
+                }
+            }
+        if (result) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
+
+
+
