ee6038a429e080daab3cb82aa6969a81d0898104
volkov.d <dmitriy.a.volkov@gmail.com>
2023-01-16 17:24:44 +0500

    Task174

---
+++ src/main/java/ru/job4j/loop/Task174.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+import java.sql.SQLOutput;
+
+public class Task174 {
+
+    public static void isPalindrome(int number) {
+        //int reverted = 0;
+        int lastNumber = number % 10;
+        int preLastnumber = number / 10;
+        System.out.println(lastNumber);
+        System.out.println(preLastnumber);
+
+    }
+
+}
