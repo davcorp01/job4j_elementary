76863a393e3602620144592ee6602c25e5ca4267
volkov.d <dmitriy.a.volkov@gmail.com>
2023-01-24 18:31:10 +0500

    Task174

--- src/main/java/ru/job4j/loop/Task174.java
+++ src/main/java/ru/job4j/loop/Task174.java
@@ -6,10 +6,16 @@ public class Task174 {
 
     public static void isPalindrome(int number) {
         //int reverted = 0;
-        int lastNumber = number % 10;
-        int preLastnumber = number / 10;
-        System.out.println(lastNumber);
-        System.out.println(preLastnumber);
+        //String s = String.valueOf(number);
+        //int remainder = 0;
+        int reverted = number % 10;
+        int remainder = number / 10;
+        for (int i = 0; reverted < remainder; i++) {
+            reverted = remainder % 10;
+            remainder = remainder / 10;
+        }
+        /*System.out.println(lastNumber);
+        System.out.println(preLastnumber);*/
 
     }
 
