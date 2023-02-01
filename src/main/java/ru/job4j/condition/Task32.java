663bcc82087dedf712316beac2634ed676ff0395
volkov.d <dmitriy.a.volkov@gmail.com>
2023-02-01 11:12:15 +0500

    Task 32 исправления (Операторы ветвления)

--- src/main/java/ru/job4j/condition/Task32.java
+++ src/main/java/ru/job4j/condition/Task32.java
@@ -6,7 +6,7 @@ public class Task32 {
             System.out.println(num1 - num2);
         } else if (num1 < num2) {
             System.out.println(num2 - num1);
-        } else if (num1 == num2) {
+        } else {
             System.out.println(num2 * num1);
         }
     }
