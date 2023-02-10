9aa5e3d8394364e9ca7b337554dfa2133136cdb8
volkov.d <dmitriy.a.volkov@gmail.com>
2023-02-10 16:29:39 +0500

    Task 46 (Операторы ветвления)

---
+++ src/main/java/ru/job4j/condition/Task46.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Task46 {
+    public static void changeNums(int num1, int num2) {
+        if (num1 > num2) {
+            System.out.println(num1 - 1);
+            System.out.println(num2 + 1);
+        } else if (num1 < num2) {
+            System.out.println(num1 + 1);
+            System.out.println(num2 - 1);
+        } else if (num1 == num2) {
+            System.out.println(num1 + 1);
+            System.out.println(num2 + 1);
+        }
+    }
+}
+
