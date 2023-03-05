52aee055c2319d7d86763bfac7bdea568e7a5e4b
volkov.d <dmitriy.a.volkov@gmail.com>
2023-03-06 03:01:56 +0500

    Task 53 решена (Операторы ветвления)

--- src/main/java/ru/job4j/condition/Task53.java
+++ src/main/java/ru/job4j/condition/Task53.java
@@ -2,14 +2,19 @@ package ru.job4j.condition;
 
 public class Task53 {
     public static void transformFraction(int num1, int num2) {
-        if (num1 > num2) {
-            int whole = (num1 / num2);
-            double quotient = ((double) num1 / (double) num2 - (double) whole) * num2;
-            System.out.println(whole + " " + quotient + "/" + num2);
-        } else if (num1 == num2) {
-            System.out.println(1);
-        } else if (num1 < num2) {
-            System.out.println("Подсчет не производится");
+        double test = (double) num1 / (double) num2 * 10 % 10;
+        if (test != 0) {
+            if (num1 > num2) {
+                int whole = (num1 / num2);
+                int quotient = num1 - num2;
+                System.out.println(whole + " " + quotient + "/" + num2);
+            } else if (num1 == num2) {
+                System.out.println(1);
+            } else {
+                System.out.println("Подсчет не производится");
+            }
+        } else {
+            System.out.println(num1 / num2);
         }
     }
 }
